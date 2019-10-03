ea182ed9055b579fc8baf4970712a1692695f5cb
neemiasbsilva <neemiasbsilva@gmail.com>
2019-10-03 17:42:35 -0400

    Implemeting to web scraping introduction using python

---
+++ into_web_scraping.py
@@ -0,0 +1,37 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+def main():
+    my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=Graphics%20Card'
+
+    # opening up connection, grabbing the page
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+    page_soup.h1
+    page_soup.p
+    
+    # grabs each product
+    containers = page_soup.findAll("div", {"class": "item-container"})
+    # how many objects find
+    print(len(containers))
+    
+    for container in containers:
+        brand = container.a.img["title"]
+        title_container = container.findAll("a", {"class": "item-title"})
+        product_name = title_container[0].text
+        shipping_container = container.findAll("li", {"class": "price-ship"})
+        shipping = shipping_container[0].text.strip()
+
+        print("brand: "+brand)
+        print("product_name: "+product_name)
+        print("shipping: "+ shipping)
+
+
+
+if __name__ == '__main__':
+    main()
